50a0b544633ae92a53b63e63e4e3f8382ae1666a
ibnuirfan <70266733+ibnuirfan@users.noreply.github.com>
2020-09-30 19:32:24 +0800

    assignment 3

--- External JS/basics.js
+++ External JS/basics.js
@@ -8,35 +8,35 @@ function basics() {
      var number2 = 2;
 
      //Arithmetic operator
-    // var sum = number + number2;
-    // var subtractedValue = number - number2;
-    // var multiplied = number * number2;
-    // console.log('Addition: ' + sum);
-    // console.log('Subtraction: ' + subtractedValue);
-    // console.log('Multiply: ' + multiplied);
-    // console.log('Increment: ' + number+5);
-    // console.log('Increment: ' + ++number);
-    // number = number + 5;
-    // //number++ -> number = number + 1
-    // number+=5;
-    // console.log('Increment: ' + ++number);
+     var sum = number + number2;
+     var subtractedValue = number - number2;
+     var multiplied = number * number2;
+     console.log('Addition: ' + sum);
+     console.log('Subtraction: ' + subtractedValue);
+     console.log('Multiply: ' + multiplied);
+     console.log('Increment: ' + number+5);
+     console.log('Increment: ' + ++number);
+     number = number + 5;
+     number++ -> number = number + 1
+     number+=5;
+     console.log('Increment: ' + ++number);
 
-    // //Conditional Operators
-    // console.log(number==number2);
-    // console.log(number!=number2);
+     //Conditional Operators
+     console.log(number==number2);
+     console.log(number!=number2);
 
-    // //Assignment Operators
-    // console.log(number);
-    // number+=1;
-    // console.log(number);
-    // number-=3;
-    // console.log(number);
-    // var number3 = 2;
-    // var modulus = 10;
-    // console.log(number3%10);
-    // console.log(number3/10);
-    // number3+=number; //number3 = number3 + number
-    // console.log(number3);
+     //Assignment Operators
+     console.log(number);
+     number+=1;
+     console.log(number);
+     number-=3;
+     console.log(number);
+     var number3 = 2;
+     var modulus = 10;
+     console.log(number3%10);
+     console.log(number3/10);
+     number3+=number; //number3 = number3 + number
+     console.log(number3);
 
     //There are multiple conditions that you want to test against
     //1. there are two numbers, check if each is greater than 1
---
+++ Module 2 Assignments/Assignment 3/numberarray.js
@@ -0,0 +1,11 @@
+
+function sumofarray(array_1,array_2){
+    var sum = [];
+    for ( var i = 0; i < Math.max(array_1.length, array_2.length); i++){
+        sum.push((array_1[i] || 0) + (array_2[i] || 0));
+    }
+    return document.getElementById('result').innerText= 'the result will be:'+ ' ' + sum;
+}
+
+
+
